4de897a861055d955a2861703f143fcf62df91c3
XPerianer <dominik.meier@student.hpi.de>
2020-06-03 14:46:27 +0200

    Reorder and add parsing tests

---
+++ src/execution.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+def textToBool(t: str):
+    if t == "failed" or t == "skipped": # If not executed, we tread it as false
+        return False
+    elif t == "passed": 
+        return True
+    else:
+        raise Exception("Value of outcome could not be interpreted as true or false. t was: {}".format(t))
+
+
+@dataclass
+class Execution:
+    outcome : bool = None
+    test_id : int = None
+    full_name : str = None
+    name : str = None
+    filepath : str = None
+    duration : float = None
+    
+    setup_outcome : bool = None
+    setup_duration : float = None
+    
+    call_outcome : bool = None
+    call_duration : float = None
+    
+    teardown_outcome : bool = None
+    teardown_duration : float = None
+        
+    @classmethod
+    def fromJson(self, json):
+        t = Execution()
+        t.outcome = textToBool(json["outcome"])
+        t.test_id = json["run_index"]
+        t.full_name = json["name"]
+        [t.filepath, t.name] = t.full_name.split('::', 1)
+        t.duration = json["duration"]
+
+        t.setup_outcome = textToBool(json["setup"]["outcome"])
+        t.setup_duration = json["setup"]["duration"]
+
+        if t.setup_outcome:
+            t.call_outcome = textToBool(json["call"]["outcome"])
+            t.call_duration = json["call"]["duration"]
+            if t.call_outcome:
+                t.teardown_outcome = textToBool(json["teardown"]["outcome"])
+                t.teardown_duration = json["teardown"]["duration"]
+        return t
---
+++ src/mutant.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+@dataclass
+class Mutant:
+    mutant_id : int = -1
+    modified_file_path : str = ''
+    line_number_changed : int = -1
+    previous_line : str = ''
+    current_line : str = ''
+
