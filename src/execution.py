851b46b4acf2ab8a5e660e18d40fe1ea074a79fd
XPerianer <dominik.meier@student.hpi.de>
2020-06-03 14:55:47 +0200

    Format

--- src/execution.py
+++ src/execution.py
@@ -1,38 +1,44 @@
 from dataclasses import dataclass
+
+
 def textToBool(t: str):
-    if t == "failed" or t == "skipped": # If not executed, we tread it as false
+    if t == "failed" or t == "skipped":  # If not executed, we tread it as false
         return False
-    elif t == "passed": 
+    elif t == "passed":
         return True
     else:
-        raise Exception("Value of outcome could not be interpreted as true or false. t was: {}".format(t))
+        raise Exception(
+            "Value of outcome could not be interpreted as true or false. t was: {}".format(
+                t
+            )
+        )
 
 
 @dataclass
 class Execution:
-    outcome : bool = None
-    test_id : int = None
-    full_name : str = None
-    name : str = None
-    filepath : str = None
-    duration : float = None
-    
-    setup_outcome : bool = None
-    setup_duration : float = None
-    
-    call_outcome : bool = None
-    call_duration : float = None
-    
-    teardown_outcome : bool = None
-    teardown_duration : float = None
-        
+    outcome: bool = None
+    test_id: int = None
+    full_name: str = None
+    name: str = None
+    filepath: str = None
+    duration: float = None
+
+    setup_outcome: bool = None
+    setup_duration: float = None
+
+    call_outcome: bool = None
+    call_duration: float = None
+
+    teardown_outcome: bool = None
+    teardown_duration: float = None
+
     @classmethod
     def fromJson(self, json):
         t = Execution()
         t.outcome = textToBool(json["outcome"])
         t.test_id = json["run_index"]
         t.full_name = json["name"]
-        [t.filepath, t.name] = t.full_name.split('::', 1)
+        [t.filepath, t.name] = t.full_name.split("::", 1)
         t.duration = json["duration"]
 
         t.setup_outcome = textToBool(json["setup"]["outcome"])
--- tests/test_execution.py
+++ tests/test_execution.py
@@ -3,7 +3,23 @@ import json
 
 from src.execution import Execution
 
+
 def test_parsing():
-    json_test = json.loads('{"name": "tests/test_appctx.py::test_basic_url_generation", "duration": 1, "run_index": 0, "setup": {"name": "setup", "duration": 2, "outcome": "passed"}, "call": {"name": "call", "duration": 3, "outcome": "passed"}, "teardown": {"name": "teardown", "duration": 4, "outcome": "passed"}, "outcome": "passed"}')
-    execution_reference = Execution(full_name= 'tests/test_appctx.py::test_basic_url_generation', duration= 1, test_id= 0, setup_duration= 2, setup_outcome= True, call_duration=3, call_outcome = True, teardown_duration = 4, teardown_outcome= True, outcome=True, name='test_basic_url_generation', filepath='tests/test_appctx.py')
-    assert(Execution.fromJson(json_test) == execution_reference)
+    json_test = json.loads(
+        '{"name": "tests/test_appctx.py::test_basic_url_generation", "duration": 1, "run_index": 0, "setup": {"name": "setup", "duration": 2, "outcome": "passed"}, "call": {"name": "call", "duration": 3, "outcome": "passed"}, "teardown": {"name": "teardown", "duration": 4, "outcome": "passed"}, "outcome": "passed"}'
+    )
+    execution_reference = Execution(
+        full_name="tests/test_appctx.py::test_basic_url_generation",
+        duration=1,
+        test_id=0,
+        setup_duration=2,
+        setup_outcome=True,
+        call_duration=3,
+        call_outcome=True,
+        teardown_duration=4,
+        teardown_outcome=True,
+        outcome=True,
+        name="test_basic_url_generation",
+        filepath="tests/test_appctx.py",
+    )
+    assert Execution.fromJson(json_test) == execution_reference
